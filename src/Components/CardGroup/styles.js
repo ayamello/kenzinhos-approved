c764614bebd61294bfcf96db6eb89a59268f1b72
Gabriela Rodrigues <gabrielarodrigues432@gmail.com>
2021-08-12 18:34:41 -0300

    feat: component
    container

---
+++ src/Components/CardGroup/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+export const ContainerGoals = styled.div`
+  min-height: 200px;
+`;
+export const ContainerActivities = styled.div`
+  min-height: 200px;
+`;
