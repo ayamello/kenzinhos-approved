fb97c22325ae5ad34e322fa4c277c8a3f690d81b
@alesilvasp <alesilvasp@gmail.com>
2021-08-16 12:08:28 -0300

    style(dashboard): add tags and search bar on Dashboard Profile

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,13 +1,62 @@
 import NavbarDash from "../../Components/NavbarDash";
 import Habits from "../../Components/Habits";
+import { Content } from "./style";
+import api from "../../Services/api";
+import { useEffect } from "react";
+import { useState } from "react";
+import jwtDecode from "jwt-decode";
+import Tags from "../../Components/Tags";
+import { Container } from "../../Pages/Groups/style";
 
 const Dashboard = () => {
-    return(
-        <div>
-            <NavbarDash />
-            <Habits/>
+  const [token] = useState(
+    JSON.parse(localStorage.getItem("@Kenzinho:token")) || ""
+  );
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+
+  const decoded = jwtDecode(token);
+
+  useEffect(() => {
+    api
+      .get(`users/${decoded.user_id}/`)
+      .then((response) => {
+        setUser(response.data.username);
+        setEmail(response.data.email);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <Container>
+      <NavbarDash />
+
+      <Content>
+        <div className="Header">
+          <span>
+            Bem vindo,
+            <strong>
+              {"  "}
+              {user}
+            </strong>
+          </span>
+          <span className='Email'>{email}</span>
         </div>
-    );
+
+        <div className="Groups">
+          <div className="GroupsList">
+            <div className="List">
+              <Tags />
+            </div>
+          </div>
+
+          <div className="GroupDetails">
+            <Habits />
+          </div>
+        </div>
+      </Content>
+    </Container>
+  );
 };
 
 export default Dashboard;
